1f951c297ac1f1293f653b93f0d26a23b36b2e12
princeh23 <zihanwangcs@163.com>
2022-10-22 19:41:14 +0800

    add builder design pattern

---
+++ 03_Builder/golang_builder.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+// ResourcePoolConfigOption option
+type ResourcePoolConfigOption struct {
+	maxTotal int
+	maxIdle  int
+	minIdle  int
+}
+
+// ResourcePoolConfigOptFunc to set option
+type ResourcePoolConfigOptFunc func(option *ResourcePoolConfigOption)
+
+func NewResourcePoolConfig(name string, opts ...ResourcePoolConfigOptFunc) (*ResourcePoolConfig, error) {
+	if name == "" {
+		return nil, fmt.Errorf("name can not be empty")
+	}
+
+	option := &ResourcePoolConfigOption{
+		maxTotal: 10,
+		maxIdle:  9,
+		minIdle:  1,
+	}
+
+	for _, opt := range opts {
+		opt(option)
+	}
+
+	if option.maxTotal < 0 || option.maxIdle < 0 || option.minIdle < 0 {
+		return nil, fmt.Errorf("args err, option: %v", option)
+	}
+
+	if option.maxTotal < option.maxIdle || option.minIdle > option.maxIdle {
+		return nil, fmt.Errorf("args err, option: %v", option)
+	}
+
+	return &ResourcePoolConfig{
+		name:     name,
+		maxTotal: option.maxTotal,
+		maxIdle:  option.maxIdle,
+		minIdle:  option.minIdle,
+	}, nil
+}
+
+func main() {
+	opt := func(option *ResourcePoolConfigOption) {
+		option.minIdle = 2
+	}
+	config, _ := NewResourcePoolConfig("test", opt)
+	fmt.Println(config)
+
+}
