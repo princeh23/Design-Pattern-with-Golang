11790805541e24035d75a49907a5563330e7325d
princeh23 <zihanwangcs@163.com>
2022-11-14 14:55:34 +0800

    add facade desgin pattern

---
+++ 09_Facade/facade.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type ProductSystem struct {
+}
+
+func (p *ProductSystem) GetProductInfo() {
+	fmt.Println("获取到商品信息")
+}
+
+type StockSystem struct {
+}
+
+func (s *StockSystem) GetStockInfo() {
+	fmt.Println("获取到库存信息")
+}
+
+type PromotionSystem struct {
+}
+
+func (p *PromotionSystem) GetPromotionInfo() {
+	fmt.Println("获取到营销信息")
+}
+
+func ProductDetail() {
+	product := &ProductSystem{}
+	stock := &StockSystem{}
+	promotion := &PromotionSystem{}
+	product.GetProductInfo()
+	stock.GetStockInfo()
+	promotion.GetPromotionInfo()
+	fmt.Println("整理完成商品详情页所有数据")
+}
+
+func main() {
+	ProductDetail()
+}
